7d8931ddcaf23d5d7a5a1111efee779eeb2e328a
vikasuttangi <vikasuttangi@gmail.com>
2025-04-24 11:14:57 +0530

    save file

---
+++ Arthamatic.java
@@ -0,0 +1,10 @@
+class arthamatic {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        int sub = a - b;
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + sub);
+    }
+}
