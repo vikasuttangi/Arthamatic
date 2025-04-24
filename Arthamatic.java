4bd30603348f4f5474ddd6a65c1a122ce985c2d8
vikasuttangi <vikasuttangi@gmail.com>
2025-04-24 11:27:26 +0530

    save file

--- Arthamatic.java
+++ Arthamatic.java
@@ -4,7 +4,11 @@ class arthamatic {
         int b = 20;
         int sum = a + b;
         int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
         System.out.println("Addition: " + sum);
         System.out.println("Subtraction: " + sub);
+        System.out.println("Multiplication: " + mul);
+        System.out.println("Division: " + div);
     }
 }
